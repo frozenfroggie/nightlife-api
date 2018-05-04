e4aca5383957ab4dc754da206f13a017e201e4c6
frozenfroggie <wojtyraj@gmail.com>
2018-05-04 21:42:57 +0200

    Update server url

--- routes/users.js
+++ routes/users.js
@@ -29,7 +29,7 @@ router.post('/', function(req, res) {
       return verificationToken.generate(user._id);
     })
     .then(verificationToken => {
-      const url = `http://nightlife.us-east-2.elasticbeanstalk.com/users/confirmation/${verificationToken}`;
+      const url = `https://api.gonightlife.tk/users/confirmation/${verificationToken}`;
       const sgMail = require('@sendgrid/mail');
       sgMail.setApiKey(process.env.SENDGRID_API_KEY);
       const msg = {
